f27fc5da797bd52fcf30ff793bf783a7cd575321
刘伟 <904368266@qq.com>
2017-05-09 16:26:11 +0800

    路径

--- service-worker.js
+++ service-worker.js
@@ -7,18 +7,18 @@ const
   offlineURL = '/offline/',
   installFilesEssential = [
     '/',
-    '/manifest.json',
-    '/css/styles.css',
-    '/js/main.js',
-    '/js/offlinepage.js',
-    '/images/logo/logo152.png'
+    'manifest.json',
+    'css/styles.css',
+    'js/main.js',
+    'js/offlinepage.js',
+    'images/logo/logo152.png'
   ].concat(offlineURL),
   installFilesDesirable = [
-    '/favicon.ico',
-    '/images/logo/logo016.png',
-    '/images/hero/power-pv.jpg',
-    '/images/hero/power-lo.jpg',
-    '/images/hero/power-hi.jpg'
+    'favicon.ico',
+    'images/logo/logo016.png',
+    'images/hero/power-pv.jpg',
+    'images/hero/power-lo.jpg',
+    'images/hero/power-hi.jpg'
   ];
 
 // install static assets
